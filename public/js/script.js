fba69d0caa8d8e54b73da64ae4d432f6ca963fd7
SOM-0x3B1 <somadagaszkar@gmail.com>
2022-12-07 20:25:11 +0100

    Add screen dragging

--- public/js/script.js
+++ public/js/script.js
@@ -6,16 +6,122 @@ function makeRows(rows, cols) {
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
         //cell.innerText = (c + 1);
-        
 
-        if(!Math.floor(Math.random()*30)){
+
+        if (!rnd(30)) {
             let image = document.createElement("img");
-            image.src = 'assets/terrain/trees.png';
+            image.src = `assets/terrain/trees0${rnd(5) + 1}.png`;
             cell.appendChild(image);
         }
-        
-        container.appendChild(cell).className = "grid-item";        
+
+        container.appendChild(cell).className = "grid-item";
     };
 };
 
 makeRows(50, 50);
+
+
+function rnd(num) {
+    return Math.floor(Math.random() * num);
+}
+
+
+
+
+const mainDisplay = document.getElementById("mainDisplay");
+
+var scale = 1,
+    panning = false,
+    pointX = 0,
+    pointY = 0,
+    start = { x: 0, y: 0 };
+
+
+function setTransform() {
+    mainDisplay.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
+}
+
+mainDisplay.onmousedown = function (e) {
+    e.preventDefault();
+    start = { x: e.clientX - pointX, y: e.clientY - pointY };
+    panning = true;
+}
+
+mainDisplay.onmouseup = function (e) {
+    panning = false;
+}
+
+mainDisplay.onmousemove = function (e) {
+    e.preventDefault();
+    if (!panning) {
+        return;
+    }
+    pointX = (e.clientX - start.x);
+    pointY = (e.clientY - start.y);
+    setTransform();
+}
+
+mainDisplay.onwheel = function (e) {
+    e.preventDefault();
+    var xs = (e.clientX - pointX) / scale,
+        ys = (e.clientY - pointY) / scale,
+        delta = (e.wheelDelta ? e.wheelDelta : -e.deltaY);
+    (delta > 0) ? (scale *= 1.2) : (scale /= 1.2);
+    pointX = e.clientX - xs * scale;
+    pointY = e.clientY - ys * scale;
+
+    setTransform();
+}
+
+
+// Make the DIV element draggable:
+/*dragElement(mainDisplay);
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    elmnt.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+
+document.addEventListener('wheel', function (e) {
+    e.preventDefault();
+    if (e.deltaY / 120 < 0 && zoomAmount < 200)
+        zoomAmount += 5;
+    else if (zoomAmount > 20)
+        zoomAmount -= 5;
+
+    mainDisplay.style.top = (mainDisplay.offsetTop - zoomAmount) + "px";
+    mainDisplay.style.left = (mainDisplay.offsetLeft - zoomAmount) + "px";
+
+    mainDisplay.style.transform = `translate3d(-50%, -50%, 0px) scale(${zoomAmount}%)`;
+});*/
