6ede97fd686ccb1fefdd14a6f55c0fc23d487cb7
SOM-0x3B1 <somadagaszkar@gmail.com>
2023-02-16 00:41:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,42 @@
-import { createServer as HTTP_Server } from 'http';
+import https from "https";
 import express, { urlencoded } from 'express';
-/*import { join } from 'path';
-
+import { join } from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';*/
+import { dirname } from 'path';
+import fs from 'fs';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const privateKey = fs.readFileSync('cert/onekilobit.eu-key.pem', 'utf8');
+const certificate = fs.readFileSync('cert/onekilobit.eu-chain.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 
 const app = express();
-const http = HTTP_Server(app);
+
+
+let visits = 0;
 
 app.disable('x-powered-by')
 
-app.use(urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(urlencoded({ extended: true, limit: '3mb' }));
 
 app.get('/', (_, res) => {
-	res.sendFile('index.html');
+	visits++;
+	console.log(visits);
+	res.sendFile(join(__dirname, '/public/index.html'));
 });
 
 app.get('*', (req, res) => {
-	res.sendFile(req.path);
+	let hasExtension = req.path.includes('.');
+	let joinedPath = join(__dirname, '/public/', hasExtension ? req.path : req.path + '.html');
+
+	if (fs.existsSync(joinedPath))
+		res.sendFile(joinedPath);
+	else {
+		res.writeHead(307, { 'Location': 'https://www.onekilobit.eu/404' });
+		res.end();
+	}
 });
 
-http.listen(80);
+https.createServer(credentials, app).listen(4453, () => { });
